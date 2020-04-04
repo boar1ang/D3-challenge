96793b496bc8f93c7fafee339cc883472bc4768a
boar1ang <inq.ops@gmail.com>
2020-04-03 21:02:46 -0500

    Apr3 files added.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,65 +1,145 @@
 // D3-Challenge Homework 16
-
-//Set up scatter plot
+//Set up dimensions for the SVG container/canvas:
 var svgWidth = 850;
-var svgHeight = 500;
+var svgHeight = 585;
 
-var margin = { top: 10, bottom: 5, left: 10, right: 10 };
+//Define chart's margins as an object:
+var margin = { top: 30, bottom: 30, left: 25, right: 15 };
 
+//Define dimensions of chart area:
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
+//Create wrapper, select chart location, append SVG area to it, & set the dimensions:
 var svg = d3
-    .select("body")
+    .select("#scatter")
     .append("svg")
     .attr("width", svgWidth)
-    .attr("height", svgHeight);
+    .attr("height", svgHeight)
+    .attr("class", "chart");
 
+
+//Append a chartGroup to the SVG area & offset it right & down using the transform attribute:
 var chartGroup = svg.append("g")
-    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+.attr("transform", `translate(${margin.left}, ${margin.top})`)
 
-    d3.select("#scatter")
-      .attr("classed", "tooltip")
-      .style("opacity", 0.5);
 
-/* id,state,abbr,poverty,povertyMoe,age,ageMoe,income,incomeMoe,healthcare,healthcareLow,healthcareHigh,
-obesity,obesityLow,obesityHigh,smokes,smokesLow,smokesHigh,-0.385218228 */
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 
 //Read in CSV file to fetch data:
-d3.csv("../assets/data/data.csv").then(function(stateHealthData) {
+d3.csv("assets/data/data.csv").then(function(stateData, err) 
+{   if (err) throw err;      
+    //Check that data logs to console:
+    console.log(stateData); 
+      
+    //Assign smoker, obesity, poverty & state variables:
+        stateData.forEach(function(data) {
+            data.id = +data.id;
+            data.stateAbbr = data.stateAbbr;
+            data.smokes = +data.smokes;        
+            data.income = +data.income;
+            data.obesity = +data.obesity;
+            data.poverty = +data.poverty;
+            console.log(data);       
+        });
+        
+    //Set ranges:
+    var x = d3.scaleLinear()
+        .range([0, width]);
+    var y = d3.scaleLinear()
+        .range([height, 0]);
+
+    //Get xMin & xMax values:
+    let xMax = d3.max(stateData, d => d.income);
+        console.log(xMax);
+    let xMin = d3.min(stateData, d => d.income);
+        console.log(xMin);
+    let yMax = d3.max(stateData, d => d.smokes);
+        console.log(yMax);
     
-    console.log(stateHealthData); 
+    //Define scale functions:
+    x.domain([xMin * 0.95, xMax * 1.1]);
+    y.domain([0, yMax * 1.1]);
+        
+    //Set default values to display on initial load:
+    let defaultXAxis = "income";
+    let defaultYAxis = "smokes";
     
-    //Smokers vs. Obesity 
-    stateHealthData.forEach(function(data) {
-        data.smokes = +data.smokes;
-        var smokers = data.smokes;
-        data.obesity = +data.obesity;
-        var obese = data.obesity;
-        var state = data.state;         
-    });
+    // var valueline = d3.line()
+    //     .x(function(d) { 
+    //         return x(d.income);
+    //         })
+    //     .y(function(d) {
+    //         return y(d.smokes);
+    //     });
+    
+    //Create axis functions:
+    let bottomAxis = d3.axisBottom(x);
+    let leftAxis = d3.axisLeft(y)
+        .ticks(7);
 
-    //Define scales, axes, max values:
-    var xScale = d3.scaleLinear().range([0, width]);
-    var yScale = d3.scaleLinear().range([height, 0]);
+    //Append X & Y axes:
+    var xAxis = chartGroup.append("g")
+        .classed("x-axis", true)
+        .attr("transform", `translate(0, ${height})`)
+        .text("Median Income")
+        .call(bottomAxis);  
 
-    var bottomAxis = d3.axisBottom(xScale);
-    var leftAxis = d3.axisLeft(yScale);
+    var yAxis = chartGroup.append("g")
+        .classed("y-axis", true)
+        .text("% Smokers")
+        .call(leftAxis);
 
-    var xMax = d3.max(stateHealthData, d => d.smokes);
-    var yMax = d3.max(stateHealthData, d => d.obesity);
+    
+    //Create circles:    
+    var circlesGroup = chartGroup.selectAll("circle")
+        .data(stateData)
+        .enter()
+        .append("circle")
+        .attr("cx", d => x(d[defaultXAxis]))
+        .attr("cy", d => y(d[defaultYAxis]))
+        .attr("r", 12)
+        .attr("fill", "#B2D8E5")
+        .attr("opacity", "0.75");
 
-    //Create tooltip
+    var circTextGroup = chartGroup.selectAll("text")
+        .data(stateData)
+        .enter()
+        .append("text")
+        .attr("class", "stateText")
+        .attr("stroke", "black")
+        .text(d => (d.stateAbbr))
+        .attr("x", d => x(d[defaultXAxis]))
+        .attr("y", d => y(d[defaultYAxis]))
+        .style("stroke", "black")
+        .attr("class", "active:Hover")
+        .style("text-anchor", "middle");
+    
+    chartGroup.append("text")
+        .attr("y", 0 - margin.left + 40)
+        .attr("x", 0 - (height /2))
+        .attr("class", "stateText")
+        .attr("text-anchor", "middle")
+        .text("Income and Smoking by State")
+        //.call(toolTip);       
 
-
-
-
-/*.catch(function(error) {
-        console.log(error);
-}) */
-
+    
+    //Set alt values:
+    var altXAxis = "poverty";
+    var altYAxis = "obesity";
+    
+    var toolTip = d3.select("body")
+        .append("div")
+        .attr("class", "d3-tip");
+    
+    circlesGroup.on("mousover", function(d, i) {
+        toolTip.style("display", "block");
+        toolTip.html(`${d.stateAbbr}`);
+    })
+    
+})
+.catch(function(error) {
+    console.log(error);
 });
-
-
-
-
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
